6c923cb787cbbb9ed39e9e5babc3b2e9d742345a
alvaroreig <argregma@gmail.com>
2013-06-29 13:28:55 +0200

    better argument processing

--- incremental_backup.sh
+++ incremental_backup.sh
@@ -2,20 +2,19 @@
 
 ############################################################################
 # Simple backup script. It uses complete and incremental backups, with     #
-# hard links to simulate snapshots. It accepts two source directories #
-# as arguments. Usage:                                                     #
+# hard links to simulate snapshots. $FULL_BACKUP_LIMIT controls the        #
+# frecuency of full backups.It accepts at least one source directory and a #
+# single destination directory as arguments. Usage:                        #
 #                                                                          #
-# incremental_backup.sh SOURCE_DIRECTORY_1 OURCE_DIRECTORY_2							 #
+# incremental_backup.sh SOURCE_DIRECTORY_1 [SOURCE_DIRECTORY_2..N]  			 #
+#       DESTINATION_DIRECTORY                                              #
 #                                                                          #
 #  Todo:                                                                   #
-# - Better input arguments management                                      #
-# - Logging                                                                #
 # - Delete old backups                                                     #
 # - Better exclusion management                                            #
+# - Simulation flag                                                        #
 ############################################################################
 
-SOURCE_DIR=$1" "$2
-DEST_DIR=/media/400gb/backup/rsync
 DATE=`date +%Y%m%d` 
 TIMESTAMP=$(date +%m%d%y%H%M%S) 
 FULL_BACKUP_STRING=backup-full-$DATE-$TIMESTAMP
@@ -23,10 +22,42 @@ INC_BACKUP_STRING=backup-inc-$DATE-$TIMESTAMP
 FULL_BACKUP_LIMIT=6
 LOG=/var/log/backup.log
 
+echo ""
+echo ""
+echo "[" `date +%Y-%m-%d_%R` "]" "###### Starting backup #######" >> $LOG
 
 
+############################################################################
+# Arguments processing. The last argument is the destination directory, the#
+# previous arguments are the source[s] directory[ies]                      #
+############################################################################
 
-echo "[" `date +%Y-%m-%d_%R` "]" "###### Starting backup #######" >> /var/log/backup.log
+ARGS=("$@")
+
+if [ ${#ARGS[*]} -lt 2 ]; then
+  echo "At least two arguments are needed" >> $LOG
+  echo "Usage: bash incremental_backup [SOURCE_DIR_1]...[SOURCE_DIR_N] [DESTINATION_DIR]" >> $LOG
+else
+
+  #Store the destination directory
+  DEST_DIR=${ARGS[${#ARGS[*]}-1]}
+
+  #Store the first source directory
+  SOURCE_DIRS=${ARGS[0]}
+  let LAST_SOURCE_POSITION=${#ARGS[*]}-2
+  SOURCE_COUNTER=1
+  
+  #Store the next source directories
+  while [ $SOURCE_COUNTER -le $LAST_SOURCE_POSITION ]; do
+    CURRENT_SOURCE_DIR=${ARGS[$SOURCE_COUNTER]-1]}
+    let SOURCE_COUNTER=SOURCE_COUNTER+1
+    SOURCE_DIRS=$SOURCE_DIRS" "$CURRENT_SOURCE_DIR
+  done
+
+  echo "Directories to backup" $SOURCE_DIRS >> $LOG
+  echo "Destination directory" $DEST_DIR >> $LOG
+fi
+
 ############################################################################
 # Browse previous backups                                                  #
 ############################################################################
@@ -37,13 +68,11 @@ BACKUPS_LIST=()
 for x in $BACKUPS
 do
     BACKUPS_LIST[$BACKUP_COUNTER]="$x"
-    #echo "[" `date +%Y-%m-%d_%R` "]" "backup detected:" ${BACKUPS_LIST[$BACKUP_COUNTER]}
     echo "[" `date +%Y-%m-%d_%R` "]" "backup detected:" ${BACKUPS_LIST[$BACKUP_COUNTER]} >> $LOG
     let BACKUP_COUNTER=BACKUP_COUNTER+1 
     
 done
 
-#echo "[" `date +%Y-%m-%d_%R` "]" "Number of previous backups: " ${#BACKUPS_LIST[*]}
 echo "[" `date +%Y-%m-%d_%R` "]" "Number of previous backups: " ${#BACKUPS_LIST[*]} >> $LOG
 
 
@@ -72,10 +101,10 @@ done
 
 if [ $NEXT_BACKUP_FULL == true ]; then
 	echo "[" `date +%Y-%m-%d_%R` "]" "The next backup will be full" >> $LOG
-	rsync -h -ab --stats --exclude '.cache/' --exclude '.thumbnails/' --exclude '.gvfs' --delete $SOURCE_DIR $DEST_DIR/$FULL_BACKUP_STRING >> $LOG
+	rsync -h -ab --stats --exclude '.cache/' --exclude '.thumbnails/' --exclude '.gvfs' --delete $SOURCE_DIRS $DEST_DIR/$FULL_BACKUP_STRING >> $LOG
 else
 	echo "[" `date +%Y-%m-%d_%R` "]" "The next backup will be incremental" >> $LOG
-	rsync -h -ab --stats --exclude '.cache/' --exclude '.thumbnails/' --exclude '.gvfs' --delete --link-dest=$DEST_DIR/$LAST_FULL_BACKUP $SOURCE_DIR $DEST_DIR/$INC_BACKUP_STRING >> $LOG
+	rsync -h -ab --stats --exclude '.cache/' --exclude '.thumbnails/' --exclude '.gvfs' --delete --link-dest=$DEST_DIR/$LAST_FULL_BACKUP $SOURCE_DIRS $DEST_DIR/$INC_BACKUP_STRING >> $LOG
 fi
 
 ############################################################################
@@ -85,6 +114,10 @@ fi
 STATUS=$?
 if [ $? -ne 0 ]; then
 	echo "[" `date +%Y-%m-%d_%R` "]" "####### Error during the backup. Please execute the script with the -v flag #######" >> $LOG
+  echo ""
+  echo ""
 else
 	echo "[" `date +%Y-%m-%d_%R` "]" "####### Backup correct #######" >> $LOG
+  echo ""
+  echo ""
 fi
