c90394a0dc3facfaea2cbe3d5451cab38578473b
Álvaro Reig González <alvaroreig@gmail.com>
2015-03-25 10:55:35 +0100

    after getting ignored packages list

--- linux_mint_unattended_upgrades/linux_mint_unattended_upgrades.sh
+++ linux_mint_unattended_upgrades/linux_mint_unattended_upgrades.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+LINUTMINTPACKAGERULESFILE="/usr/lib/linuxmint/mintUpdate/rules"
+PACKAGELEVELLIMIT=3
 startdelimiterkeyword="PROGRAM: "
 enddelimiterkeyword=" INSTALLED: "
 
@@ -8,6 +10,42 @@ strindex() {
     [[ $x = $1 ]] && echo -1 || echo ${#x}
   }
 
+
+
+#################################################################################
+# Fount out packages that should not be upgraded, according to 'the Mint way',  #
+# and store them in the array 'ignoredpackages'                                 #
+#################################################################################
+ignoredpackages=()
+mapfile -t packageslist < <(cat $LINUTMINTPACKAGERULESFILE)
+
+for packageline in "${packageslist[@]}"
+do
+    # Each line is of this form: banshee|*|2||
+    #echo "$packageline"
+    IFS='|' read -ra parts <<< "$packageline"
+
+    if [ "${parts[2]}" -gt $PACKAGELEVELLIMIT ]; then
+        echo "[" `date +%Y-%m-%d_%R` "]" "Package like ${parts[0]} won't be updated"
+        ignoredpackages=(${ignoredpackages[@]} "${parts[0]}")
+    else
+      echo "[" `date +%Y-%m-%d_%R` "]" "Package like ${parts[0]} can be updated"
+    fi
+done
+
+# Output ignored packages list
+for element in "${ignoredpackages[@]}"
+do
+    # Each line is of this form: PROGRAM: xorg INSTALLED: 1:7.7+1ubuntu8 AVAILABLE: 1:7.7+1ubuntu8.1
+    echo "$element"
+done
+
+exit 0
+
+
+#################################################################
+# Find packages that could be upgraded                          #
+#################################################################
 list=$(apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "PROGRAM: $1 INSTALLED: $2 AVAILABLE: $3\n"}')
 #echo $list
 
